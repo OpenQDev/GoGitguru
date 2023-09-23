3965668212769ddf54ab3a0a1a4f12104056b7c1
FlacoJones <andrew@openq.dev>
2023-09-22 20:11:59 -0500

    refactors out deleteLocalRepoAndTarball

--- main.go
+++ main.go
@@ -30,8 +30,7 @@ func main() {
 
 	// At the end of function execution, delete the repo and .tar.gz repo from the local filesystem
 	// The great thing with this defer is it will run regardless of the outcomes of subsequent subprocesses
-	defer os.RemoveAll(repo)
-	defer os.RemoveAll(repo + ".tar.gz")
+	defer deleteLocalRepoAndTarball(repo)
 
 	// Clone the git repo
 	fmt.Printf("\033[94mCloning https://github.com/%s/%s.git...\033[0m\n", organization, repo)
@@ -99,4 +98,16 @@ func uploadRepoToS3(organization string, repo string) error {
 		Body:   tarball,
 	})
 	return err
+}
+
+func deleteLocalRepoAndTarball(repo string) error {
+	err := os.RemoveAll(repo)
+	if err != nil {
+		return err
+	}
+	err = os.RemoveAll(repo + ".tar.gz")
+	if err != nil {
+		return err
+	}
+	return nil
 }
