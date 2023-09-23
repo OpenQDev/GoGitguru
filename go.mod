c4d69a0ba2f91e1f4a7bd084bb05ab8fe71b6c2a
FlacoJones <andrew@openq.dev>
2023-09-23 14:23:55 -0500

    adds go-sqlmock to provide a mock sql db for the add handler

--- go.mod
+++ go.mod
@@ -3,12 +3,18 @@ module main
 go 1.21.1
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/aws/aws-sdk-go v1.45.15
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/cors v1.2.1
-	github.com/google/uuid v1.3.1
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
+	github.com/stretchr/testify v1.8.4
 )
 
-require github.com/jmespath/go-jmespath v0.4.0 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ handler_add_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"main/internal/database"
+	"main/internal/pkg/logger"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddHandler(t *testing.T) {
+	// Initialize a new instance of apiConfig with mocked DB
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		logger.LogFatalRedAndExit("can't create mock DB:", err)
+	}
+
+	// Expectations and actions for the mock DB can be defined here
+	// For example:
+	// mock.ExpectQuery("^SELECT (.+) FROM users$").WillReturnRows(sqlmock.NewRows([]string{"id", "name"}).AddRow(1, "John"))
+
+	queries := database.New(db)
+
+	apiCfg := apiConfig{
+		DB: queries,
+	}
+
+	// Define test cases
+	tests := []struct {
+		name           string
+		repoUrls       []string
+		expectedStatus int
+	}{
+		{
+			name:           "Valid repo URLs",
+			repoUrls:       []string{"https://github.com/org/repo1", "https://github.com/org/repo2"},
+			expectedStatus: http.StatusAccepted,
+		},
+		// Add more test cases as needed
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Prepare the HTTP request
+			body, _ := json.Marshal(map[string][]string{
+				"repo_urls": tt.repoUrls,
+			})
+			req, _ := http.NewRequest("POST", "/add", bytes.NewBuffer(body))
+			rr := httptest.NewRecorder()
+
+			// Call the handler function
+			apiCfg.addHandler(rr, req)
+
+			// Check the status code
+			assert.Equal(t, tt.expectedStatus, rr.Code)
+
+			// Check if there were any unexpected calls to the mock DB
+			if err := mock.ExpectationsWereMet(); err != nil {
+				t.Errorf("there were unfulfilled expectations: %s", err)
+			}
+		})
+	}
+}
