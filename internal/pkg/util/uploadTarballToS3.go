1bdf52a49afeb8d73ebc06629be81215692653bf
FlacoJones <andrew@openq.dev>
2023-09-22 22:10:58 -0500

    refactors uploadTarballToS3 to separate file

---
+++ internal/pkg/util/uploadTarballToS3.go
@@ -0,0 +1,52 @@
+package util
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+func UploadTarballToS3(prefixPath string, organization string, repo string) error {
+	// Get AWS API key and secret from environment variables
+	awsAccessKeyID := os.Getenv("AWS_ACCESS_KEY_ID")
+	awsSecretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+	// Create a session using SharedConfigEnable
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("us-east-2"),
+		Credentials: credentials.NewStaticCredentials(awsAccessKeyID, awsSecretAccessKey, ""),
+	},
+	)
+	if err != nil {
+		return err
+	}
+
+	// Create an uploader with the session and default options
+	uploader := s3manager.NewUploader(sess)
+
+	// Create a tarball of the .git directory
+	err = exec.Command("tar", "-czf", filepath.Join(prefixPath, repo+".tar.gz"), filepath.Join(prefixPath, repo+"/.git")).Run()
+	if err != nil {
+		return err
+	}
+
+	// Open the tarball
+	tarball, err := os.Open(filepath.Join(prefixPath, repo+".tar.gz"))
+	if err != nil {
+		return err
+	}
+
+	// Upload the file to S3
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String("openqrepos"),
+		Key:    aws.String(fmt.Sprintf("%s/%s.tar.gz", organization, repo)),
+		Body:   tarball,
+	})
+	return err
+}
--- main.go
+++ main.go
@@ -5,13 +5,7 @@ import (
 	"log"
 	"main/internal/pkg/util"
 	"os"
-	"os/exec"
-	"path/filepath"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/joho/godotenv"
 )
 
@@ -48,48 +42,9 @@ func main() {
 	// Upload the repo to S3
 	fmt.Printf("\033[94mUploading %s/%s to S3...\033[0m", organization, repo)
 	fmt.Println()
-	err = uploadRepoToS3(prefixPath, organization, repo)
+	err = util.UploadTarballToS3(prefixPath, organization, repo)
 	if err != nil {
 		log.Fatalf("\033[91mFailed to upload to S3: %s\033[0m", err)
 	}
 	fmt.Printf("\033[32m%s/%s uploaded to S3!\033[0m\n", organization, repo)
-}
-
-func uploadRepoToS3(prefixPath string, organization string, repo string) error {
-	// Get AWS API key and secret from environment variables
-	awsAccessKeyID := os.Getenv("AWS_ACCESS_KEY_ID")
-	awsSecretAccessKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
-
-	// Create a session using SharedConfigEnable
-	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String("us-east-2"),
-		Credentials: credentials.NewStaticCredentials(awsAccessKeyID, awsSecretAccessKey, ""),
-	},
-	)
-	if err != nil {
-		return err
-	}
-
-	// Create an uploader with the session and default options
-	uploader := s3manager.NewUploader(sess)
-
-	// Create a tarball of the .git directory
-	err = exec.Command("tar", "-czf", filepath.Join(prefixPath, repo+".tar.gz"), filepath.Join(prefixPath, repo+"/.git")).Run()
-	if err != nil {
-		return err
-	}
-
-	// Open the tarball
-	tarball, err := os.Open(filepath.Join(prefixPath, repo+".tar.gz"))
-	if err != nil {
-		return err
-	}
-
-	// Upload the file to S3
-	_, err = uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String("openqrepos"),
-		Key:    aws.String(fmt.Sprintf("%s/%s.tar.gz", organization, repo)),
-		Body:   tarball,
-	})
-	return err
 }
