be3ec1e780f65fd9e35a5903773915e23870bfb8
FlacoJones <andrew@openq.dev>
2023-09-22 19:40:50 -0500

    extract org to var; better string formatting, better error logging

--- main.go
+++ main.go
@@ -17,22 +17,21 @@ func main() {
 	// Initialize go dotenv
 	err := godotenv.Load()
 	if err != nil {
-		fmt.Println("Error loading .env file")
-		log.Fatal(err)
+		log.Fatal("Error loading .env file: ", err)
 	}
 
 	// Define the repo name
-	repoName := "OpenQ-Workflows"
+	repo := "OpenQ-Workflows"
+	organization := "OpenQDev"
+
+	// At the end of function execution, delete the repo and .tar.gz repo from the local filesystem
+	defer os.RemoveAll(repo)
+	defer os.RemoveAll(repo + ".tar.gz")
 
 	// Clone the git repo
-
-	defer os.RemoveAll(repoName)
-	defer os.RemoveAll(repoName + ".tar.gz")
-
-	_, err = exec.Command("git", "clone", "https://github.com/OpenQDev/"+repoName+".git").Output()
+	_, err = exec.Command("git", "clone", fmt.Sprintf("https://github.com/%s/%s.git", organization, repo)).Output()
 	if err != nil {
-		fmt.Println("Failed to clone")
-		log.Fatal(err)
+		log.Fatal("Failed to clone: ", err)
 	}
 
 	// Get AWS API key and secret from environment variables
@@ -45,40 +44,34 @@ func main() {
 		Credentials: credentials.NewStaticCredentials(awsAccessKeyID, awsSecretAccessKey, ""),
 	},
 	)
-
 	if err != nil {
-		fmt.Println("Failed to start AWS session")
-		log.Fatal(err)
+		log.Fatal("Failed to start AWS session:", err)
 	}
 
 	// Create an uploader with the session and default options
 	uploader := s3manager.NewUploader(sess)
 
 	// Create a tarball of the .git directory
-	err = exec.Command("tar", "-czf", repoName+".tar.gz", repoName+"/.git").Run()
+	err = exec.Command("tar", "-czf", repo+".tar.gz", repo+"/.git").Run()
 	if err != nil {
-		fmt.Println("Failed to create tarball")
-		log.Fatal(err)
+		log.Fatal("Failed to create tarball:", err)
 	}
 
 	// Open the tarball
-	tarball, err := os.Open(repoName + ".tar.gz")
+	tarball, err := os.Open(repo + ".tar.gz")
 	if err != nil {
-		fmt.Println("Failed to open tarball")
-		log.Fatal(err)
+		log.Fatal("Failed to open tarball:", err)
 	}
 
 	// Upload the file to S3
 	_, err = uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String("openqrepos"),
-		Key:    aws.String(repoName + ".tar.gz"),
+		Key:    aws.String(repo + ".tar.gz"),
 		Body:   tarball,
 	})
 	if err != nil {
-		fmt.Println("Failed to upload to S3")
-		log.Fatal(err)
+		log.Fatal("Failed to upload to S3:", err)
 	}
 
 	// The file is automatically closed by the uploader
-
 }
