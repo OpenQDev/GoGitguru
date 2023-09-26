efca8f6701e6cc5c5dfb136708083a416ab2afa1
FlacoJones <andrew@openq.dev>
2023-09-26 14:04:38 -0500

    adds check that it's in a git repository

--- internal/pkg/gitutil/GitLogCsv.go
+++ internal/pkg/gitutil/GitLogCsv.go
@@ -1,26 +1,33 @@
 package gitutil
 
 // git -C . log --reverse --pretty=format:'%H-;-%an-;-%ae-;-%at-;-%ct%n%s' --numstat --since=2020-01-01
+// git -C . rev-parse --is-inside-work-tree
 
 import (
-	"log"
+	"main/internal/pkg/logger"
 	"os/exec"
 	"path/filepath"
 )
 
 func GitLogCsv(prefixPath string, repo string, fromCommitDate string) []byte {
+	fullRepoPath := filepath.Join(prefixPath, repo)
+
+	cmdCheck := exec.Command("git", "-C", fullRepoPath, "rev-parse", "--is-inside-work-tree")
+	err := cmdCheck.Run()
+	if err != nil {
+		logger.LogFatalRedAndExit("%s/%s is not a git repository: %s", prefixPath, repo, err)
+	}
+
 	if fromCommitDate == "" {
 		fromCommitDate = "2020-01-01"
 	}
-
-	fullRepoPath := filepath.Join(prefixPath, repo)
 
 	cmd := exec.Command("git", "-C", fullRepoPath, "log", "--reverse", "--pretty=format:'%H-;-%an-;-%ae-;-%at-;-%ct%n%s'", "--numstat", "--since="+fromCommitDate)
 
 	out, err := cmd.Output()
 
 	if err != nil {
-		log.Fatal(err)
+		logger.LogFatalRedAndExit("error running git log in %s: %s", fullRepoPath, err)
 	}
 
 	return out
---
+++ sql/schema/003_commits.sql
@@ -0,0 +1,20 @@
+-- +goose Up
+
+CREATE TABLE IF NOT EXISTS commits (
+    commit_hash VARCHAR(40) NOT NULL,
+    author VARCHAR(120),
+    author_email VARCHAR(255),
+    author_date BIGINT,
+    committer_date BIGINT,
+    message TEXT,
+    insertions INT,
+    deletions INT,
+    lines_changed INT GENERATED ALWAYS AS (insertions + deletions) STORED,
+    files_changed INT,
+    repo_url VARCHAR(150) REFERENCES repo_urls(url),
+    UNIQUE(commit_hash, repo_url)
+);
+
+-- +goose Down
+
+DROP TABLE commits;
