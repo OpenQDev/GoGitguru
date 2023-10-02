94a8eead564662b54b6337d89eac1a5514d8ae22
FlacoJones <andrew@openq.dev>
2023-10-02 14:06:42 -0500

    bubble up the bad commit for logging

--- internal/pkg/gitutil/processRepo.go
+++ internal/pkg/gitutil/processRepo.go
@@ -7,17 +7,20 @@ import (
 )
 
 func ProcessRepo(prefixPath string, repo string, repoUrl string, db *database.Queries) {
+	// Set repo status to storing_commits
 	db.UpdateStatusAndUpdatedAt(context.Background(), database.UpdateStatusAndUpdatedAtParams{
 		Status: database.RepoStatusStoringCommits,
 		Url:    repoUrl,
 	})
 
-	err := StoreCommits(GetFormattedGitLogs(prefixPath, repo, ""), repoUrl, db)
+	commit, err := StoreCommits(GetFormattedGitLogs(prefixPath, repo, ""), repoUrl, db)
 	if err != nil {
+		// Set repo status to failed
 		db.UpdateStatusAndUpdatedAt(context.Background(), database.UpdateStatusAndUpdatedAtParams{
 			Status: database.RepoStatusFailed,
 			Url:    repoUrl,
 		})
-		logger.LogFatalRedAndExit("Failed to insert commit: %s", err)
+
+		logger.LogFatalRedAndExit("for repository %s, failed to insert this commit: %s with the following error: ", repoUrl, commit, err)
 	}
 }
--- internal/pkg/gitutil/storeCommits.go
+++ internal/pkg/gitutil/storeCommits.go
@@ -3,15 +3,13 @@ package gitutil
 import (
 	"context"
 	"database/sql"
-	"fmt"
 	"main/internal/database"
 	"main/internal/pkg/logger"
 )
 
-func StoreCommits(gitLogs []GitLog, repoUrl string, db *database.Queries) error {
+func StoreCommits(gitLogs []GitLog, repoUrl string, db *database.Queries) (*database.InsertCommitParams, error) {
 	for _, gitLog := range gitLogs {
-		fmt.Println(gitLog)
-		err := db.InsertCommit(context.Background(), database.InsertCommitParams{
+		params := database.InsertCommitParams{
 			CommitHash:    gitLog.CommitHash,
 			Author:        sql.NullString{String: gitLog.AuthorName, Valid: gitLog.AuthorName != ""},
 			AuthorEmail:   sql.NullString{String: gitLog.AuthorEmail, Valid: gitLog.AuthorEmail != ""},
@@ -22,13 +20,15 @@ func StoreCommits(gitLogs []GitLog, repoUrl string, db *database.Queries) error 
 			Deletions:     sql.NullInt32{Int32: 0, Valid: true},
 			FilesChanged:  sql.NullInt32{Int32: 0, Valid: true},
 			RepoUrl:       sql.NullString{String: repoUrl, Valid: repoUrl != ""},
-		})
+		}
+
+		err := db.InsertCommit(context.Background(), params)
 
 		if err != nil {
-			return err
+			return &params, err
 		}
 	}
 	logger.LogBlue("Successfully stored commits in the database.")
 
-	return nil
+	return nil, nil
 }
