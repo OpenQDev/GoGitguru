4f638a88678cb8d266761c0b6d27302d1c62f268
FlacoJones <andrew@openq.dev>
2023-10-03 18:02:16 -0500

    removes unused git commands

--- internal/pkg/gitutil/gitCommands.go
+++ internal/pkg/gitutil/gitCommands.go
@@ -10,24 +10,3 @@ git clone https://github.com/OpenQDev/OpenQ-DRM repos/OpenQ-DRM
 func GitCloneCommand(cloneString string, cloneDestination string) *exec.Cmd {
 	return exec.Command("git", "clone", cloneString, cloneDestination)
 }
-
-/*
-git -C . rev-parse --is-inside-work-tree
-*/
-func CheckIsAGitRepo(fullRepoPath string) *exec.Cmd {
-	return exec.Command("git", "-C", fullRepoPath, "rev-parse", "--is-inside-work-tree")
-}
-
-/*
-git -C . log --reverse --pretty=format":%H-;-%an-;-%ae-;-%at-;-%ct%n%s" --numstat
-*/
-func GitLogCommand(fullRepoPath string, fromCommitDate string) *exec.Cmd {
-	return exec.Command("git", "-C", fullRepoPath, "log", "--reverse", "--pretty=format:%H-;-%an-;-%ae-;-%at-;-%ct%n%s", "--numstat", "--since="+fromCommitDate)
-}
-
-/*
-git -C . log -n 20 --reverse --pretty=format":%H-;-%an-;-%ae-;-%at-;-%ct%n%s" --numstat
-*/
-func GitLog20Command(fullRepoPath string, fromCommitDate string) *exec.Cmd {
-	return exec.Command("git", "-C", fullRepoPath, "log", "-n", "20", "--reverse", "--pretty=format:%H-;-%an-;-%ae-;-%at-;-%ct%n%s", "--numstat", "--since="+fromCommitDate)
-}
