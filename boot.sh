83d69d4c2c5cae49870e7c6e472b6e3e398faa3a
mktcode <kontakt@markus-kottlaender.de>
2023-10-04 13:57:12 +0200

    more boot.sh

--- boot.sh
+++ boot.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-docker stop postgres 2>/dev/null || true
-docker rm postgres 2>/dev/null || true
+echo "Removing existing postgres container"
+docker stop gitguru-postgres > /dev/null 2>/dev/null
+docker rm gitguru-postgres > /dev/null 2>/dev/null
 
-docker run --name postgres -d -e POSTGRES_HOST_AUTH_METHOD=trust -p 5432:5432 postgres
+echo "Starting new container: $(docker run --name gitguru-postgres -d -e POSTGRES_HOST_AUTH_METHOD=trust -p 5432:5432 postgres)"
 
 # Wait for PostgreSQL to become accessible
 until PGPASSWORD= psql -h "localhost" -U "postgres" -p 5432 -c '\q' 2>/dev/null; do
-	echo "Postgres is unavailable - sleeping"
+	echo "Waiting for postgres"
 	sleep 1
 done
 
-echo "Postgres is up - executing command"
+echo "Postgres is up - running migrations"
 
 cd ./sql/schema
 goose postgres "postgres://postgres:@localhost:5432/postgres?sslmode=disable" up
