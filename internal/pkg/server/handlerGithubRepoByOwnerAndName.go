fb2f77b48ea3a357e968b8b45e8bccf84d2a87f2
FlacoJones <andrew@openq.dev>
2023-10-04 15:25:47 -0500

    unmarshalls json into type Repo

--- internal/pkg/server/handlerGithubRepoByOwnerAndName.go
+++ internal/pkg/server/handlerGithubRepoByOwnerAndName.go
@@ -1,21 +1,23 @@
 package server
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 
 	"github.com/go-chi/chi"
 )
 
 type Repo struct {
-	GithubRestID    int    `json:"github_rest_id"`
-	GithubGraphqlID string `json:"github_graphql_id"`
+	GithubRestID    int    `json:"id"`
+	GithubGraphqlID string `json:"node_id"`
 	URL             string `json:"url"`
 	Name            string `json:"name"`
 	FullName        string `json:"full_name"`
 	Private         bool   `json:"private"`
-	OwnerLogin      string `json:"owner_login"`
-	OwnerAvatarURL  string `json:"owner_avatar_url"`
+	Owner           struct {
+		Login     string `json:"login"`
+		AvatarURL string `json:"avatar_url"`
+	} `json:"owner"`
 	Description     string `json:"description"`
 	Homepage        string `json:"homepage"`
 	Fork            bool   `json:"fork"`
@@ -39,41 +41,38 @@ type Repo struct {
 }
 
 func (apiConfig *ApiConfig) HandlerGithubRepoByOwnerAndName(w http.ResponseWriter, r *http.Request) {
+	githubAccessToken := r.Header.Get("GH-Authorization")
+
+	if githubAccessToken == "" {
+		RespondWithError(w, 400, "You must provide a GitHub access token.")
+		return
+	}
+
 	owner := chi.URLParam(r, "owner")
 	name := chi.URLParam(r, "name")
 
-	fmt.Println(owner)
-	fmt.Println(name)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.github.com/repos/"+owner+"/"+name, nil)
+	if err != nil {
+		RespondWithError(w, 500, "Failed to create request.")
+		return
+	}
 
-	// githubAccessToken := r.Header.Get("GH-Authorization")
+	req.Header.Add("Authorization", "token "+githubAccessToken)
+	resp, err := client.Do(req)
+	if err != nil {
+		RespondWithError(w, 500, "Failed to make request.")
+		return
+	}
 
-	// if githubAccessToken == "" {
-	// 	RespondWithError(w, 400, "You must provide a GitHub access token.")
-	// 	return
-	// }
+	defer resp.Body.Close()
 
-	// client := &http.Client{}
-	// req, err := http.NewRequest("GET", "https://api.github.com/repos/"+owner+"/"+name, nil)
-	// if err != nil {
-	// 	RespondWithError(w, 500, "Failed to create request.")
-	// 	return
-	// }
+	var repo Repo
+	err = json.NewDecoder(resp.Body).Decode(&repo)
+	if err != nil {
+		RespondWithError(w, 500, "Failed to decode response.")
+		return
+	}
 
-	// req.Header.Add("Authorization", "token "+githubAccessToken)
-	// resp, err := client.Do(req)
-	// if err != nil {
-	// 	RespondWithError(w, 500, "Failed to make request.")
-	// 	return
-	// }
-
-	// defer resp.Body.Close()
-
-	// var repo Repo
-	// err = json.NewDecoder(resp.Body).Decode(&repo)
-	// if err != nil {
-	// 	RespondWithError(w, 500, "Failed to decode response.")
-	// 	return
-	// }
-
-	RespondWithJSON(w, 200, struct{}{})
+	RespondWithJSON(w, 200, repo)
 }
