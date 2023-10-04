4ed25caef9f0ab4b944dbd29e4fe923ef69f001e
mktcode <kontakt@markus-kottlaender.de>
2023-10-04 13:16:48 +0200

    don't print expected errors in boot.sh

--- boot.sh
+++ boot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-docker stop postgres
-docker rm postgres
+docker stop postgres 2>/dev/null || true
+docker rm postgres 2>/dev/null || true
 
 docker run --name postgres -d -e POSTGRES_HOST_AUTH_METHOD=trust -p 5432:5432 postgres
 
 # Wait for PostgreSQL to become accessible
-until PGPASSWORD= psql -h "localhost" -U "postgres" -p 5432 -c '\q'; do
+until PGPASSWORD= psql -h "localhost" -U "postgres" -p 5432 -c '\q' 2>/dev/null; do
 	echo "Postgres is unavailable - sleeping"
 	sleep 1
 done
