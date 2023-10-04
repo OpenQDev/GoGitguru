34a28a9a4c31462f7fb4d65b83ca87450fbbe22b
FlacoJones <andrew@openq.dev>
2023-10-04 18:28:07 -0500

    moves models to models.go

--- internal/pkg/server/handlerGithubUserByLogin.go
+++ internal/pkg/server/handlerGithubUserByLogin.go
@@ -11,27 +11,6 @@ import (
 	"github.com/go-chi/chi"
 )
 
-type User struct {
-	InternalID      int    `json:"internal_id"`
-	GithubRestID    int    `json:"id"`
-	GithubGraphqlID string `json:"node_id"`
-	Login           string `json:"login"`
-	Name            string `json:"name"`
-	Email           string `json:"email"`
-	AvatarURL       string `json:"avatar_url"`
-	Company         string `json:"company"`
-	Location        string `json:"location"`
-	Bio             string `json:"bio"`
-	Blog            string `json:"blog"`
-	Hireable        bool   `json:"hireable"`
-	TwitterUsername string `json:"twitter_username"`
-	Followers       int    `json:"followers"`
-	Following       int    `json:"following"`
-	Type            string `json:"type"`
-	CreatedAt       string `json:"created_at"`
-	UpdatedAt       string `json:"updated_at"`
-}
-
 func (apiConfig *ApiConfig) HandlerGithubUserByLogin(w http.ResponseWriter, r *http.Request) {
 	githubAccessToken := r.Header.Get("GH-Authorization")
 
