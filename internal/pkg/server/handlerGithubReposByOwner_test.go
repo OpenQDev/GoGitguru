a5088314ccd61b9f63ca3d23471860f74ff78b71
FlacoJones <andrew@openq.dev>
2023-10-07 18:50:54 -0500

    uses JSON file for expectations

--- internal/pkg/server/handlerGithubReposByOwner_test.go
+++ internal/pkg/server/handlerGithubReposByOwner_test.go
@@ -1,13 +1,15 @@
 package server
 
 import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
 	"io"
 	"main/internal/database"
 	"main/internal/pkg/logger"
 	"net/http"
 	"net/http/httptest"
 	"os"
-	"strings"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -24,24 +26,24 @@ func TestHandlerGithubReposByOwner(t *testing.T) {
 	// Initialize queries with the mocked DB collection.
 	queries := database.New(db)
 
+	// Read the JSON file
+	jsonFile, err := os.Open("mockReposReturn.json")
+	if err != nil {
+		t.Errorf("Failed to open mockReposReturn.json: %s", err)
+		return
+	}
+	defer jsonFile.Close()
+
+	// Parse the JSON file into a slice of RestRepo
+	byteValue, _ := io.ReadAll(jsonFile)
+	var repos []RestRepo
+	json.Unmarshal(byteValue, &repos)
+
+	fmt.Println(repos)
+
 	// Create a mock of Github REST API
 	mux := http.NewServeMux()
 	mux.HandleFunc("/users/", func(w http.ResponseWriter, r *http.Request) {
-		// Check if the request path is correct
-		if !strings.Contains(r.URL.Path, "/repos") {
-			http.NotFound(w, r)
-			return
-		}
-
-		// Read the JSON file
-		jsonFile, err := os.Open("mockReposReturn.json")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer jsonFile.Close()
-
-		// Write the JSON file to the response
 		io.Copy(w, jsonFile)
 	})
 
@@ -88,9 +90,35 @@ func TestHandlerGithubReposByOwner(t *testing.T) {
 
 			rr := httptest.NewRecorder()
 
-			// Define your expectations here. For example, if you expect the InsertGithubRepo function to be called,
-			// you can set it up like this:
-			// mock.ExpectExec("^-- name: InsertGithubRepo :one.*").WithArgs(...).WillReturnResult(sqlmock.NewResult(1, 1))
+			mock.ExpectExec("^-- name: InsertGithubRepo :one.*").WithArgs(
+				repos[0].ID,                              // GithubRestID
+				repos[0].NodeID,                          // GithubGraphqlID
+				"https://github.com/octocat/Hello-World", // Url
+				"Hello-World",                            // Name
+				"octocat/Hello-World",                    // FullName
+				false,                                    // Private
+				"octocat",                                // OwnerLogin
+				"https://github.com/images/error/octocat_happy.gif", // OwnerAvatarUrl
+				"This your first repo!",                             // Description
+				"",                                                  // Homepage
+				false,                                               // Fork
+				9,                                                   // ForksCount
+				false,                                               // Archived
+				false,                                               // Disabled
+				"mit",                                               // License
+				"Ruby",                                              // Language
+				80,                                                  // StargazersCount
+				80,                                                  // WatchersCount
+				9,                                                   // OpenIssuesCount
+				true,                                                // HasIssues
+				false,                                               // HasDiscussions
+				true,                                                // HasProjects
+				sql.NullTime{},                                      // CreatedAt
+				sql.NullTime{},                                      // UpdatedAt
+				sql.NullTime{},                                      // PushedAt
+				"public",                                            // Visibility
+				108,                                                 // Size
+				"master").WillReturnResult(sqlmock.NewResult(1, 1))
 
 			// Call the handler function
 			apiCfg.HandlerGithubReposByOwner(rr, req)
