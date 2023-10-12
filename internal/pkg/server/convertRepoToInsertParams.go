9157e21a5580100768a6679be7f693522689e4f5
FlacoJones <andrew@openq.dev>
2023-10-11 20:52:58 -0500

    extracts ConvertRestRepoToInsertParams

---
+++ internal/pkg/server/convertRepoToInsertParams.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"database/sql"
+	"main/internal/database"
+	"time"
+)
+
+func ConvertRestRepoToInsertParams(repo GithubRestRepo) database.InsertGithubRepoParams {
+	createdAt, _ := time.Parse(time.RFC3339, repo.CreatedAt)
+	updatedAt, _ := time.Parse(time.RFC3339, repo.UpdatedAt)
+	pushedAt, _ := time.Parse(time.RFC3339, repo.PushedAt)
+
+	return database.InsertGithubRepoParams{
+		GithubRestID:    int32(repo.GithubRestID),
+		GithubGraphqlID: repo.GithubGraphqlID,
+		Url:             repo.URL,
+		Name:            repo.Name,
+		FullName:        repo.FullName,
+		Private:         sql.NullBool{Bool: repo.Private, Valid: true},
+		OwnerLogin:      repo.Owner.Login,
+		OwnerAvatarUrl:  sql.NullString{String: repo.Owner.AvatarURL, Valid: true},
+		Description:     sql.NullString{String: repo.Description, Valid: true},
+		Homepage:        sql.NullString{String: "homepage", Valid: true},
+		Fork:            sql.NullBool{Bool: repo.Fork, Valid: true},
+		ForksCount:      sql.NullInt32{Int32: int32(repo.ForksCount), Valid: true},
+		Archived:        sql.NullBool{Bool: repo.Archived, Valid: true},
+		Disabled:        sql.NullBool{Bool: repo.Disabled, Valid: true},
+		License:         sql.NullString{String: "license", Valid: true},
+		Language:        sql.NullString{String: "language", Valid: true},
+		StargazersCount: sql.NullInt32{Int32: int32(repo.StargazersCount), Valid: true},
+		WatchersCount:   sql.NullInt32{Int32: int32(repo.WatchersCount), Valid: true},
+		OpenIssuesCount: sql.NullInt32{Int32: int32(repo.OpenIssuesCount), Valid: true},
+		HasIssues:       sql.NullBool{Bool: repo.HasIssues, Valid: true},
+		HasDiscussions:  sql.NullBool{Bool: repo.HasDiscussions, Valid: true},
+		HasProjects:     sql.NullBool{Bool: repo.HasProjects, Valid: true},
+		CreatedAt:       sql.NullTime{Time: createdAt, Valid: true},
+		UpdatedAt:       sql.NullTime{Time: updatedAt, Valid: true},
+		PushedAt:        sql.NullTime{Time: pushedAt, Valid: true},
+		Visibility:      sql.NullString{String: repo.Visibility, Valid: true},
+		Size:            sql.NullInt32{Int32: int32(repo.Size), Valid: true},
+		DefaultBranch:   sql.NullString{String: repo.DefaultBranch, Valid: true},
+	}
+}
--- internal/pkg/server/handlerGithubRepoByOwnerAndName.go
+++ internal/pkg/server/handlerGithubRepoByOwnerAndName.go
@@ -2,12 +2,8 @@ package server
 
 import (
 	"context"
-	"database/sql"
 	"encoding/json"
-	"fmt"
-	"main/internal/database"
 	"net/http"
-	"time"
 
 	"github.com/go-chi/chi"
 )
@@ -49,53 +45,8 @@ func (apiConfig *ApiConfig) HandlerGithubRepoByOwnerAndName(w http.ResponseWrite
 		return
 	}
 
-	createdAt, err := time.Parse(time.RFC3339, repo.CreatedAt)
-	if err != nil {
-		RespondWithError(w, 500, fmt.Sprintf("failed to parse createdAt: %s", repo.CreatedAt))
-		return
-	}
-	updatedAt, err := time.Parse(time.RFC3339, repo.UpdatedAt)
-	if err != nil {
-		RespondWithError(w, 500, fmt.Sprintf("failed to parse updatedAt: %s", repo.UpdatedAt))
-		return
-	}
-	pushedAt, err := time.Parse(time.RFC3339, repo.PushedAt)
-	if err != nil {
-		RespondWithError(w, 500, fmt.Sprintf("failed to parse pushedAt: %s", repo.PushedAt))
-		return
-	}
-
 	// Insert the repo into the database using sqlc generated methods
-	params := database.InsertGithubRepoParams{
-		GithubRestID:    int32(repo.GithubRestID),
-		GithubGraphqlID: repo.GithubGraphqlID,
-		Url:             repo.URL,
-		Name:            repo.Name,
-		FullName:        repo.FullName,
-		Private:         sql.NullBool{Bool: repo.Private, Valid: true},
-		OwnerLogin:      repo.Owner.Login,
-		OwnerAvatarUrl:  sql.NullString{String: repo.Owner.AvatarURL, Valid: true},
-		Description:     sql.NullString{String: repo.Description, Valid: true},
-		Homepage:        sql.NullString{String: repo.Homepage, Valid: true},
-		Fork:            sql.NullBool{Bool: repo.Fork, Valid: true},
-		ForksCount:      sql.NullInt32{Int32: int32(repo.ForksCount), Valid: true},
-		Archived:        sql.NullBool{Bool: repo.Archived, Valid: true},
-		Disabled:        sql.NullBool{Bool: repo.Disabled, Valid: true},
-		License:         sql.NullString{String: repo.License.Name, Valid: true},
-		Language:        sql.NullString{String: repo.Language, Valid: true},
-		StargazersCount: sql.NullInt32{Int32: int32(repo.StargazersCount), Valid: true},
-		WatchersCount:   sql.NullInt32{Int32: int32(repo.WatchersCount), Valid: true},
-		OpenIssuesCount: sql.NullInt32{Int32: int32(repo.OpenIssuesCount), Valid: true},
-		HasIssues:       sql.NullBool{Bool: repo.HasIssues, Valid: true},
-		HasDiscussions:  sql.NullBool{Bool: repo.HasDiscussions, Valid: true},
-		HasProjects:     sql.NullBool{Bool: repo.HasProjects, Valid: true},
-		CreatedAt:       sql.NullTime{Time: createdAt, Valid: true},
-		UpdatedAt:       sql.NullTime{Time: updatedAt, Valid: true},
-		PushedAt:        sql.NullTime{Time: pushedAt, Valid: true},
-		Visibility:      sql.NullString{String: repo.Visibility, Valid: true},
-		Size:            sql.NullInt32{Int32: int32(repo.Size), Valid: true},
-		DefaultBranch:   sql.NullString{String: repo.DefaultBranch, Valid: true},
-	}
+	params := ConvertRestRepoToInsertParams(repo)
 
 	_, err = apiConfig.DB.InsertGithubRepo(context.Background(), params)
 	if err != nil {
