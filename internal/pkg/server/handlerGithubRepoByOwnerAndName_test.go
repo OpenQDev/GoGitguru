ef067732b772f277adf31f65121c0b8c9ea51dbe
FlacoJones <andrew@openq.dev>
2023-10-14 15:55:24 -0500

    adds setupmock to separate db expectations

--- internal/pkg/server/handlerGithubRepoByOwnerAndName_test.go
+++ internal/pkg/server/handlerGithubRepoByOwnerAndName_test.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"fmt"
 	"io"
 	"main/internal/pkg/logger"
 	"main/internal/pkg/server/mocks"
@@ -12,9 +11,7 @@ import (
 	"net/http/httptest"
 	"os"
 	"testing"
-	"time"
 
-	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -79,61 +76,7 @@ func TestHandlerGithubRepoByOwnerAndName(t *testing.T) {
 
 			rr := httptest.NewRecorder()
 
-			fullName := fmt.Sprintf("%s/%s", tt.owner, tt.name)
-
-			if tt.title == "SHOULD_RETURN_REPO_IF_EXISTS_IN_DB" {
-				mock.ExpectQuery("-- name: CheckGithubRepoExists :one").WithArgs(fullName).WillReturnRows(sqlmock.NewRows([]string{"exists"}).AddRow(true))
-
-				createdAt, _ := time.Parse(time.RFC3339, repo.CreatedAt)
-				updatedAt, _ := time.Parse(time.RFC3339, repo.UpdatedAt)
-				pushedAt, _ := time.Parse(time.RFC3339, repo.PushedAt)
-
-				rows := sqlmock.NewRows([]string{"internal_id", "github_rest_id", "github_graphql_id", "url", "name", "full_name", "private", "owner_login", "owner_avatar_url", "description", "homepage", "fork", "forks_count", "archived", "disabled", "license", "language", "stargazers_count", "watchers_count", "open_issues_count", "has_issues", "has_discussions", "has_projects", "created_at", "updated_at", "pushed_at", "visibility", "size", "default_branch"}).
-					AddRow(1, repo.GithubRestID, repo.GithubGraphqlID, repo.URL, repo.Name, repo.FullName, repo.Private, repo.Owner.Login, repo.Owner.AvatarURL, repo.Description, repo.Homepage, repo.Fork, repo.ForksCount, repo.Archived, repo.Disabled, repo.License.Name, repo.Language, repo.StargazersCount, repo.WatchersCount, repo.OpenIssuesCount, repo.HasIssues, repo.HasDiscussions, repo.HasProjects, createdAt, updatedAt, pushedAt, repo.Visibility, repo.Size, repo.DefaultBranch)
-
-				mock.ExpectQuery("-- name: GetGithubRepo :one").WithArgs(fullName).WillReturnRows(rows)
-			}
-
-			if tt.title == "SHOULD_GET_REPO_FOR_ORG_AND_NAME" {
-				createdAt, _ := time.Parse(time.RFC3339, repo.CreatedAt)
-				updatedAt, _ := time.Parse(time.RFC3339, repo.UpdatedAt)
-				pushedAt, _ := time.Parse(time.RFC3339, repo.PushedAt)
-
-				rows := sqlmock.NewRows([]string{"internal_id", "github_rest_id", "github_graphql_id", "url", "name", "full_name", "private", "owner_login", "owner_avatar_url", "description", "homepage", "fork", "forks_count", "archived", "disabled", "license", "language", "stargazers_count", "watchers_count", "open_issues_count", "has_issues", "has_discussions", "has_projects", "created_at", "updated_at", "pushed_at", "visibility", "size", "default_branch"}).
-					AddRow(1, repo.GithubRestID, repo.GithubGraphqlID, repo.URL, repo.Name, repo.FullName, repo.Private, repo.Owner.Login, repo.Owner.AvatarURL, repo.Description, repo.Homepage, repo.Fork, repo.ForksCount, repo.Archived, repo.Disabled, repo.License.Name, repo.Language, repo.StargazersCount, repo.WatchersCount, repo.OpenIssuesCount, repo.HasIssues, repo.HasDiscussions, repo.HasProjects, createdAt, updatedAt, pushedAt, repo.Visibility, repo.Size, repo.DefaultBranch)
-
-				mock.ExpectQuery("-- name: CheckGithubRepoExists :one").WithArgs(fullName).WillReturnRows(sqlmock.NewRows([]string{"exists"}).AddRow(false))
-				mock.ExpectQuery("^-- name: InsertGithubRepo :one.*").WithArgs(
-					repo.GithubRestID,    // 0 - GithubRestID
-					repo.GithubGraphqlID, // 1 - GithubGraphqlID
-					repo.URL,             // 2 - Url
-					repo.Name,            // 3 - Name
-					repo.FullName,        // 4 - FullName
-					repo.Private,         // 5 - Private
-					repo.Owner.Login,     // 6 - OwnerLogin
-					repo.Owner.AvatarURL, // 7 - OwnerAvatarUrl
-					repo.Description,     // 8 - Description
-					repo.Homepage,        // 9 - Homepage
-					repo.Fork,            // 10 - Fork
-					repo.ForksCount,      // 11 - ForksCount
-					repo.Archived,        // 12 - Archived
-					repo.Disabled,        // 13 - Disabled
-					repo.License.Name,    // 14 - License
-					repo.Language,        // 15 - Language
-					repo.StargazersCount, // 16 - StargazersCount
-					repo.WatchersCount,   // 17 - WatchersCount
-					repo.OpenIssuesCount, // 18 - OpenIssuesCount
-					repo.HasIssues,       // 19 - HasIssues
-					repo.HasDiscussions,  // 20 - HasDiscussions
-					repo.HasProjects,     // 21 - HasProjects
-					createdAt,            // 22 - CreatedAt
-					updatedAt,            // 23 - UpdatedAt
-					pushedAt,             // 24 - PushedAt
-					repo.Visibility,      // 25 - Visibility
-					repo.Size,            // 26 - Size
-					repo.DefaultBranch,   // 27 - DefaultBranch
-				).WillReturnRows(rows)
-			}
+			tt.setupMock(mock, repo)
 
 			// ACT
 			apiCfg.HandlerGithubRepoByOwnerAndName(rr, req)
--- internal/pkg/server/handlerGithubRepoByOwnerAndName_testcases.go
+++ internal/pkg/server/handlerGithubRepoByOwnerAndName_testcases.go
@@ -1,6 +1,12 @@
 package server
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+)
 
 type HandlerGithubRepoByOwnerAndNameTest struct {
 	title          string
@@ -9,6 +15,7 @@ type HandlerGithubRepoByOwnerAndNameTest struct {
 	expectedStatus int
 	authorized     bool
 	shouldError    bool
+	setupMock      func(mock sqlmock.Sqlmock, repo GithubRestRepo)
 }
 
 const drmTestOrg = "DRM-Test-Organization"
@@ -23,6 +30,19 @@ func shouldReturn401() HandlerGithubRepoByOwnerAndNameTest {
 		expectedStatus: http.StatusUnauthorized,
 		authorized:     false,
 		shouldError:    true,
+		setupMock: func(mock sqlmock.Sqlmock, repo GithubRestRepo) {
+			fullName := fmt.Sprintf("%s/%s", drmTestOrg, drmTestRepo)
+			mock.ExpectQuery("-- name: CheckGithubRepoExists :one").WithArgs(fullName).WillReturnRows(sqlmock.NewRows([]string{"exists"}).AddRow(true))
+
+			createdAt, _ := time.Parse(time.RFC3339, repo.CreatedAt)
+			updatedAt, _ := time.Parse(time.RFC3339, repo.UpdatedAt)
+			pushedAt, _ := time.Parse(time.RFC3339, repo.PushedAt)
+
+			rows := sqlmock.NewRows([]string{"internal_id", "github_rest_id", "github_graphql_id", "url", "name", "full_name", "private", "owner_login", "owner_avatar_url", "description", "homepage", "fork", "forks_count", "archived", "disabled", "license", "language", "stargazers_count", "watchers_count", "open_issues_count", "has_issues", "has_discussions", "has_projects", "created_at", "updated_at", "pushed_at", "visibility", "size", "default_branch"}).
+				AddRow(1, repo.GithubRestID, repo.GithubGraphqlID, repo.URL, repo.Name, repo.FullName, repo.Private, repo.Owner.Login, repo.Owner.AvatarURL, repo.Description, repo.Homepage, repo.Fork, repo.ForksCount, repo.Archived, repo.Disabled, repo.License.Name, repo.Language, repo.StargazersCount, repo.WatchersCount, repo.OpenIssuesCount, repo.HasIssues, repo.HasDiscussions, repo.HasProjects, createdAt, updatedAt, pushedAt, repo.Visibility, repo.Size, repo.DefaultBranch)
+
+			mock.ExpectQuery("-- name: GetGithubRepo :one").WithArgs(fullName).WillReturnRows(rows)
+		},
 	}
 }
 
@@ -35,6 +55,48 @@ func shouldReturnRepoIfExistsInDb() HandlerGithubRepoByOwnerAndNameTest {
 		expectedStatus: http.StatusOK,
 		authorized:     true,
 		shouldError:    false,
+		setupMock: func(mock sqlmock.Sqlmock, repo GithubRestRepo) {
+			fullName := fmt.Sprintf("%s/%s", drmTestOrg, drmTestRepo)
+
+			createdAt, _ := time.Parse(time.RFC3339, repo.CreatedAt)
+			updatedAt, _ := time.Parse(time.RFC3339, repo.UpdatedAt)
+			pushedAt, _ := time.Parse(time.RFC3339, repo.PushedAt)
+
+			rows := sqlmock.NewRows([]string{"internal_id", "github_rest_id", "github_graphql_id", "url", "name", "full_name", "private", "owner_login", "owner_avatar_url", "description", "homepage", "fork", "forks_count", "archived", "disabled", "license", "language", "stargazers_count", "watchers_count", "open_issues_count", "has_issues", "has_discussions", "has_projects", "created_at", "updated_at", "pushed_at", "visibility", "size", "default_branch"}).
+				AddRow(1, repo.GithubRestID, repo.GithubGraphqlID, repo.URL, repo.Name, repo.FullName, repo.Private, repo.Owner.Login, repo.Owner.AvatarURL, repo.Description, repo.Homepage, repo.Fork, repo.ForksCount, repo.Archived, repo.Disabled, repo.License.Name, repo.Language, repo.StargazersCount, repo.WatchersCount, repo.OpenIssuesCount, repo.HasIssues, repo.HasDiscussions, repo.HasProjects, createdAt, updatedAt, pushedAt, repo.Visibility, repo.Size, repo.DefaultBranch)
+
+			mock.ExpectQuery("-- name: CheckGithubRepoExists :one").WithArgs(fullName).WillReturnRows(sqlmock.NewRows([]string{"exists"}).AddRow(false))
+			mock.ExpectQuery("^-- name: InsertGithubRepo :one.*").WithArgs(
+				repo.GithubRestID,    // 0 - GithubRestID
+				repo.GithubGraphqlID, // 1 - GithubGraphqlID
+				repo.URL,             // 2 - Url
+				repo.Name,            // 3 - Name
+				repo.FullName,        // 4 - FullName
+				repo.Private,         // 5 - Private
+				repo.Owner.Login,     // 6 - OwnerLogin
+				repo.Owner.AvatarURL, // 7 - OwnerAvatarUrl
+				repo.Description,     // 8 - Description
+				repo.Homepage,        // 9 - Homepage
+				repo.Fork,            // 10 - Fork
+				repo.ForksCount,      // 11 - ForksCount
+				repo.Archived,        // 12 - Archived
+				repo.Disabled,        // 13 - Disabled
+				repo.License.Name,    // 14 - License
+				repo.Language,        // 15 - Language
+				repo.StargazersCount, // 16 - StargazersCount
+				repo.WatchersCount,   // 17 - WatchersCount
+				repo.OpenIssuesCount, // 18 - OpenIssuesCount
+				repo.HasIssues,       // 19 - HasIssues
+				repo.HasDiscussions,  // 20 - HasDiscussions
+				repo.HasProjects,     // 21 - HasProjects
+				createdAt,            // 22 - CreatedAt
+				updatedAt,            // 23 - UpdatedAt
+				pushedAt,             // 24 - PushedAt
+				repo.Visibility,      // 25 - Visibility
+				repo.Size,            // 26 - Size
+				repo.DefaultBranch,   // 27 - DefaultBranch
+			).WillReturnRows(rows)
+		},
 	}
 }
 
@@ -47,6 +109,7 @@ func shouldReturnRepoForOwnerAndName() HandlerGithubRepoByOwnerAndNameTest {
 		expectedStatus: http.StatusOK,
 		authorized:     true,
 		shouldError:    false,
+		setupMock:      func(mock sqlmock.Sqlmock, repo GithubRestRepo) {},
 	}
 }
 
