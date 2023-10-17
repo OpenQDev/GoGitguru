8e854ed5c64c57d91ecd617ec85d840715dfdeb0
FlacoJones <andrew@openq.dev>
2023-10-17 15:32:33 -0500

    adds test for insertions deletions and fileschanged

--- internal/pkg/reposync/prepareCommitHistoryForBulkInsertion_test.go
+++ internal/pkg/reposync/prepareCommitHistoryForBulkInsertion_test.go
@@ -40,6 +40,9 @@ func TestPrepareCommitHistoryForBulkInsertion(t *testing.T) {
 	assert.Equal(t, []int64{1696277247, 1696277205}, commitObject.AuthorDate)
 	assert.Equal(t, []int64{1696277247, 1696277205}, commitObject.CommitterDate)
 	assert.Equal(t, []string{"updates README", "initial commit"}, commitObject.Message)
+	assert.Equal(t, []int32{1, 0}, commitObject.Insertions)
+	assert.Equal(t, []int32{0, 0}, commitObject.Deletions)
+	assert.Equal(t, []int32{1, 0}, commitObject.FilesChanged)
 	assert.Equal(t, []string{"https://github.com/OpenQDev/OpenQ-DRM-TestRepo", "https://github.com/OpenQDev/OpenQ-DRM-TestRepo"}, commitObject.RepoUrls)
 
 	// cap (size of array) and len (number of elements in array) must match - or else pq.Array will attempt to insert cap - len # of empty commits
@@ -48,6 +51,9 @@ func TestPrepareCommitHistoryForBulkInsertion(t *testing.T) {
 	assert.Equal(t, cap(commitObject.AuthorEmail), len(commitObject.AuthorEmail))
 	assert.Equal(t, cap(commitObject.AuthorDate), len(commitObject.AuthorDate))
 	assert.Equal(t, cap(commitObject.CommitterDate), len(commitObject.CommitterDate))
+	assert.Equal(t, cap(commitObject.Insertions), len(commitObject.Insertions))
+	assert.Equal(t, cap(commitObject.Deletions), len(commitObject.Deletions))
+	assert.Equal(t, cap(commitObject.FilesChanged), len(commitObject.FilesChanged))
 	assert.Equal(t, cap(commitObject.Message), len(commitObject.Message))
 	assert.Equal(t, cap(commitObject.RepoUrls), len(commitObject.RepoUrls))
 }
