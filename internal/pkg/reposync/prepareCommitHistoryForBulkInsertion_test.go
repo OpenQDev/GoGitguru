846a62f2a420737fe704ccea09a65ae2909b6eef
FlacoJones <andrew@openq.dev>
2023-10-17 15:30:32 -0500

    updates cap and len tests

--- internal/pkg/reposync/prepareCommitHistoryForBulkInsertion_test.go
+++ internal/pkg/reposync/prepareCommitHistoryForBulkInsertion_test.go
@@ -37,18 +37,17 @@ func TestPrepareCommitHistoryForBulkInsertion(t *testing.T) {
 	assert.Equal(t, []string{"06a12f9c203112a149707ff73e4298749744c358", "9fae86bc8e89895b961d81bd7e9e4e897501c8bb"}, commitObject.CommitHash)
 	assert.Equal(t, []string{"FlacoJones", "FlacoJones"}, commitObject.Author)
 	assert.Equal(t, []string{"andrew@openq.dev", "andrew@openq.dev"}, commitObject.AuthorEmail)
+	assert.Equal(t, []int64{1696277247, 1696277205}, commitObject.AuthorDate)
+	assert.Equal(t, []int64{1696277247, 1696277205}, commitObject.CommitterDate)
 	assert.Equal(t, []string{"updates README", "initial commit"}, commitObject.Message)
 	assert.Equal(t, []string{"https://github.com/OpenQDev/OpenQ-DRM-TestRepo", "https://github.com/OpenQDev/OpenQ-DRM-TestRepo"}, commitObject.RepoUrls)
 
-	assert.Equal(t, 2, cap(commitObject.CommitHash))
-	assert.Equal(t, 2, cap(commitObject.Author))
-	assert.Equal(t, 2, cap(commitObject.AuthorEmail))
-	assert.Equal(t, 2, cap(commitObject.Message))
-	assert.Equal(t, 2, cap(commitObject.RepoUrls))
-
-	assert.Equal(t, 2, len(commitObject.CommitHash))
-	assert.Equal(t, 2, len(commitObject.Author))
-	assert.Equal(t, 2, len(commitObject.AuthorEmail))
-	assert.Equal(t, 2, len(commitObject.Message))
-	assert.Equal(t, 2, len(commitObject.RepoUrls))
+	// cap (size of array) and len (number of elements in array) must match - or else pq.Array will attempt to insert cap - len # of empty commits
+	assert.Equal(t, cap(commitObject.CommitHash), len(commitObject.CommitHash))
+	assert.Equal(t, cap(commitObject.Author), len(commitObject.Author))
+	assert.Equal(t, cap(commitObject.AuthorEmail), len(commitObject.AuthorEmail))
+	assert.Equal(t, cap(commitObject.AuthorDate), len(commitObject.AuthorDate))
+	assert.Equal(t, cap(commitObject.CommitterDate), len(commitObject.CommitterDate))
+	assert.Equal(t, cap(commitObject.Message), len(commitObject.Message))
+	assert.Equal(t, cap(commitObject.RepoUrls), len(commitObject.RepoUrls))
 }
