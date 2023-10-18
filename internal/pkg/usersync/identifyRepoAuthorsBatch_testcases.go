18ce227b55da3da9ca5c611ba6dcdc11dfc855fd
FlacoJones <andrew@openq.dev>
2023-10-18 12:30:11 -0500

    extracts gql user from author

--- internal/pkg/usersync/identifyRepoAuthorsBatch_testcases.go
+++ internal/pkg/usersync/identifyRepoAuthorsBatch_testcases.go
@@ -17,56 +17,37 @@ const repoUrl = "OpenQ-Workflows"
 func identifyRepoAuthorsBatchTest1() IdentifyRepoAuthorsBatchTestCase {
 	const AUTHOR_BATCH = "AUTHOR_BATCH"
 
+	user := githubGraphQL.User{
+		GithubRestID:    93455288,
+		GithubGraphqlID: "U_kgDOBZIDuA",
+		Login:           "FlacoJones",
+		Name:            "AndrewOBrien",
+		Email:           "",
+		AvatarURL:       "https://avatars.githubusercontent.com/u/93455288?u=fd1fb04b6ff2bf397f8353eafffc3bfb4bd66e84\u0026v=4",
+		Company:         "",
+		Location:        "",
+		Hireable:        false,
+		Bio:             "builder at OpenQ",
+		Blog:            "",
+		TwitterUsername: "",
+		Followers: struct {
+			TotalCount int `json:"totalCount"`
+		}{
+			TotalCount: 12,
+		},
+		Following: struct {
+			TotalCount int `json:"totalCount"`
+		}{
+			TotalCount: 0,
+		},
+		CreatedAt: "2021-10-30T23:43:10Z",
+		UpdatedAt: "2023-10-10T15:52:33Z",
+	}
+
 	author := githubGraphQL.Author{
 		Name:  "FlacoJones",
 		Email: "andrew@openq.dev",
-		User: struct {
-			GithubRestID    int    `json:"github_rest_id"`
-			GithubGraphqlID string `json:"github_graphql_id"`
-			Login           string `json:"login"`
-			Name            string `json:"name"`
-			Email           string `json:"email"`
-			AvatarURL       string `json:"avatar_url"`
-			Company         string `json:"company"`
-			Location        string `json:"location"`
-			Hireable        bool   `json:"hireable"`
-			Bio             string `json:"bio"`
-			Blog            string `json:"blog"`
-			TwitterUsername string `json:"twitter_username"`
-			Followers       struct {
-				TotalCount int `json:"totalCount"`
-			} `json:"followers"`
-			Following struct {
-				TotalCount int `json:"totalCount"`
-			} `json:"following"`
-			CreatedAt string `json:"created_at"`
-			UpdatedAt string `json:"updated_at"`
-		}{
-			GithubRestID:    93455288,
-			GithubGraphqlID: "U_kgDOBZIDuA",
-			Login:           "FlacoJones",
-			Name:            "AndrewOBrien",
-			Email:           "",
-			AvatarURL:       "https://avatars.githubusercontent.com/u/93455288?u=fd1fb04b6ff2bf397f8353eafffc3bfb4bd66e84\u0026v=4",
-			Company:         "",
-			Location:        "",
-			Hireable:        false,
-			Bio:             "builder at OpenQ",
-			Blog:            "",
-			TwitterUsername: "",
-			Followers: struct {
-				TotalCount int `json:"totalCount"`
-			}{
-				TotalCount: 12,
-			},
-			Following: struct {
-				TotalCount int `json:"totalCount"`
-			}{
-				TotalCount: 0,
-			},
-			CreatedAt: "2021-10-30T23:43:10Z",
-			UpdatedAt: "2023-10-10T15:52:33Z",
-		},
+		User:  user,
 	}
 
 	expectedOutput := make(map[string]githubGraphQL.Commit)
@@ -76,7 +57,7 @@ func identifyRepoAuthorsBatchTest1() IdentifyRepoAuthorsBatchTestCase {
 		title:   AUTHOR_BATCH,
 		repoUrl: repoUrl,
 		authorCommitList: []AuthorCommitTuple{
-			AuthorCommitTuple{
+			{
 				Author:     "abc123@email.com",
 				CommitHash: "commitHash",
 			},
