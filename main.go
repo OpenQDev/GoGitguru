a9886c5277ad25015d4218e2e8ccb1cde8440f32
FlacoJones <andrew@openq.dev>
2023-10-21 15:14:51 -0500

    adds converted for commits endpoints

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	}
 
 	if env.SyncUsers {
-		time.Sleep(10 * time.Second)
+		time.Sleep(6 * time.Second)
 		go usersync.StartSyncingUser(database, "repos", 10, time.Duration(env.SyncUsersIntervalMinutesInt)*time.Minute, env.GhAccessToken, 2, apiCfg)
 	}
 
