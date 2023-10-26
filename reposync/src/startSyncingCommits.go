da884487b9dbbdf5eb767007bf66b735c50500ab
FlacoJones <andrew@openq.dev>
2023-10-26 17:33:56 -0500

    updates

--- reposync/src/startSyncingCommits.go
+++ reposync/src/startSyncingCommits.go
@@ -27,6 +27,7 @@ func StartSyncingCommits(
 	for _, repoUrl := range repoUrls {
 		organization, repo := gitutil.ExtractOrganizationAndRepositoryFromUrl(repoUrl)
 		logger.LogGreenDebug("processing %s/%s...", organization, repo)
+		startDate := time.Unix(1577836800, 0) // Unix time for Jan 1, 2020
 
 		// Check if the repo is present in the repos directory
 		if gitutil.IsGitRepository(prefixPath, organization, repo) {
@@ -34,25 +35,24 @@ func StartSyncingCommits(
 			logger.LogBlue("repository %s exists. pulling...", repoUrl)
 			gitutil.PullRepo(prefixPath, organization, repo)
 			logger.LogBlue("repository %s pulled!", repoUrl)
+
+			// there are cases where the repository may exist in local, but hasn't been synced
+			// no rows in result set just means it didn't have any commit entries for that repo
+			latestCommitterDate, err := db.GetLatestCommitterDate(context.Background(), repoUrl)
+
+			if err != nil {
+				if !strings.Contains(err.Error(), "sql: no rows in result set") {
+					logger.LogFatalRedAndExit("error getting latest committer date: %s ", err)
+				}
+			}
+
+			startDate = time.Unix(int64(latestCommitterDate), 0)
 		} else {
 			// If not, clone it
 			logger.LogBlue("repository %s does not exist. cloning...", repoUrl)
 			gitutil.CloneRepo(prefixPath, organization, repo)
 			logger.LogBlue("repository %s cloned!", repoUrl)
 		}
-
-		// there are cases where the repository may exist in local, but hasn't been synced
-		latestCommitterDate, err := db.GetLatestCommitterDate(context.Background(), repoUrl)
-
-		if err != nil {
-			if strings.Contains(err.Error(), "sql: no rows in result set") {
-				latestCommitterDate = 1577836800 // Unix time for Jan 1, 2020
-			} else {
-				logger.LogFatalRedAndExit("error getting latest committer date: %s ", err)
-			}
-		}
-
-		startDate := time.Unix(int64(latestCommitterDate), 0)
 
 		err = ProcessRepo(prefixPath, organization, repo, repoUrl, startDate, db)
 		if err != nil {
