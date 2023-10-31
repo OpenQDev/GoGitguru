813b3bbec6d476a6c18bd3f1420057ab343d73e4
FlacoJones <andrew@openq.dev>
2023-10-31 17:28:57 -0500

    udpates

--- util/setup/getDatabase.go
+++ util/setup/getDatabase.go
@@ -18,3 +18,12 @@ func GetDatbase(dbUrl string) (*database.Queries, error) {
 
 	return queries, nil
 }
+
+func GetSQLDatbase(dbUrl string) (*sql.DB, error) {
+	conn, err := sql.Open("postgres", dbUrl)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
