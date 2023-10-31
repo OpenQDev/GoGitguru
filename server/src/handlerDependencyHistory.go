cb3c6c583ca941cec4abb92b4c01ba97b021e350
FlacoJones <andrew@openq.dev>
2023-10-31 10:39:32 -0500

    updates handlerDependencyHistory

--- server/src/handlerDependencyHistory.go
+++ server/src/handlerDependencyHistory.go
@@ -1,7 +1,11 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
+
+	"github.com/OpenQDev/GoGitguru/util/gitutil"
+	"github.com/OpenQDev/GoGitguru/util/marshaller"
 )
 
 type DependencyHistoryRequest struct {
@@ -17,5 +21,25 @@ type DependencyHistoryResponse struct {
 
 func (apiCfg *ApiConfig) HandlerDependencyHistory(w http.ResponseWriter, r *http.Request) {
 	var dependencyHistoryResponse DependencyHistoryResponse
+
+	var body DependencyHistoryRequest
+	err := marshaller.ReaderToType(r.Body, &body)
+	if err != nil {
+		RespondWithError(w, http.StatusBadRequest, fmt.Sprintf("failed to read body of request: %s", err))
+		return
+	}
+
+	organization, repo := gitutil.ExtractOrganizationAndRepositoryFromUrl(body.RepoUrl)
+
+	repoDir := fmt.Sprintf("./repos/%s/%s", organization, repo)
+
+	commits, err := gitutil.GitDependencyHistory(repoDir, body.DependencySearched, body.FilePaths)
+	if err != nil {
+		RespondWithError(w, http.StatusInternalServerError, fmt.Sprintf("error getting dependency history: %s", err))
+		return
+	}
+
+	fmt.Println(commits)
+
 	RespondWithJSON(w, 200, dependencyHistoryResponse)
 }
