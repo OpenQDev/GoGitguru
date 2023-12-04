e4746d6a2559ba13028b37ef8ae5a791e9221523
FlacoJones <andrew@openq.dev>
2023-12-04 08:36:42 -0600

    fixes sql

--- sql/schema/006_email_indexes.sql
+++ sql/schema/006_email_indexes.sql
@@ -2,8 +2,3 @@
 
 CREATE INDEX idx_author_email ON commits (author_email);
 CREATE INDEX idx_email ON github_user_rest_id_author_emails (email);
-
--- +goose Down
-
-DROP INDEX idx_author_email ON commits;
-DROP INDEX idx_email ON github_user_rest_id_author_emails;
