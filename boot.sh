a08041a41c4660c722b362db9b774183d0819ac0
FlacoJones <andrew@openq.dev>
2024-03-06 14:19:25 -0600

    adds send_interrupt=true to .air.toml

--- boot.sh
+++ boot.sh
@@ -26,6 +26,8 @@ cd ../..
 
 # psql -h "localhost" -U "postgres" -p $POSTGRES_PORT -f ./repos.sql
 
+lsof -ti tcp:8000 | xargs kill
+
 # To install go air, a Go runtime with live reloads on code changes,
 # run the following command: go get -u github.com/cosmtrek/air
 cd $APP && air
