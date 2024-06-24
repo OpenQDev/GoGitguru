10581957598a9b4376e472b57cbaa757a2756c66
Christopher-Stevers <christopher.stevers@protonmail.com>
2024-06-24 10:03:07 -0400

    fixed server tests

--- server/src/handlerGithubUserByLogin_testcases.go
+++ server/src/handlerGithubUserByLogin_testcases.go
@@ -45,10 +45,10 @@ func valid() HandlerGithubUserByLoginTestCase {
 			createdAt, _ := time.Parse(time.RFC3339, user.CreatedAt)
 			updatedAt, _ := time.Parse(time.RFC3339, user.UpdatedAt)
 
-			rows := sqlmock.NewRows([]string{"internal_id", "github_rest_id", "github_graphql_id", "login", "name", "email", "avatar_url", "company", "location", "bio", "blog", "hireable", "twitter_username", "followers", "following", "type", "created_at", "updated_at"}).
-				AddRow(1, user.GithubRestID, user.GithubGraphqlID, strings.ToLower(user.Login), user.Name, user.Email, user.AvatarURL, user.Company, user.Location, user.Bio, user.Blog, user.Hireable, user.TwitterUsername, user.Followers, user.Following, user.Type, createdAt, updatedAt)
+			rows := sqlmock.NewRows([]string{"internal_id"}).
+				AddRow(1)
 
-			mock.ExpectQuery("-- name: CheckGithubUserExists :one").WithArgs(user.Login).WillReturnRows(sqlmock.NewRows([]string{"exists"}).AddRow(false))
+			mock.ExpectQuery("-- name: CheckGithubUserExists :one").WithArgs(user.Login).WillReturnRows(sqlmock.NewRows([]string{"exists"}).AddRow(0))
 			mock.ExpectQuery("^-- name: InsertUser :one.*").WithArgs(
 				user.GithubRestID,
 				user.GithubGraphqlID,
